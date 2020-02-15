49b20fa2009e1384e732bcc2b0122ad9b8df9d65
Anthony Vo <antvo2448@hotmail.com>
2020-02-15 16:43:26 -0500

    modified serializer construction

--- src/main/java/frc/robot/subsystems/serializer/DummySerializer.java
+++ src/main/java/frc/robot/subsystems/serializer/DummySerializer.java
@@ -7,26 +7,14 @@
 
 package frc.robot.subsystems.serializer;
 
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.ControlType;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.serializer.*;
 import frc.robot.Robot;
-import frc.robot.RobotConstants2020;
 
 /**
  * Add your docs here.
  */
 public class DummySerializer extends SubsystemBase implements ISerializer {
 
-  public DummySerializer(Robot.RobotType robotType) {
-
-  }
-  
   public void driveTurnTable(SerializerSpeed speed, boolean ejectorOn) {
   }
 }
