8600981b996d15512d161ce642ed0e4df4b613ea
croooutons <mkruscica0@gmail.com>
2020-02-26 20:30:29 -0500

    added camera and aim turret auto functions

--- src/main/java/frc/robot/auto/commands/AimTurret.java
+++ src/main/java/frc/robot/auto/commands/AimTurret.java
@@ -7,36 +7,47 @@
 
 package frc.robot.auto.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
 import frc.robot.auto.ParseCommand;
 
 public class AimTurret extends BaseAutoCommand {
 
   public static final String COMMAND_NAME = "aimTurret";
+  double limelightXAngle, limelightYAngle;
+  private double panTrim = -2.5;
 
   public AimTurret(ParseCommand parsedCommand) {
     super(parsedCommand);
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis); 
+    addRequirements(Robot.turret);
   }
 
   // Called just before this Command runs the first time
   @Override
   public void initialize() {
+    super.initialize();
+    Robot.turret.setPanTarget(Robot.turret.getPanAngle());
+    Robot.turret.setTiltTargetAngle(Robot.turret.getTiltAngle());
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   public void execute() {
-  }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    if (Robot.camera.hasTarget()) {
+      limelightYAngle = Robot.camera.getYAngle();
 
-  // Called once after isFinished returns true or when interrupted
-  @Override
-  public void end(boolean interrupted) {
+      var targetData = Robot.flywheelTable.getIdealTarget(limelightYAngle);
+      Robot.turret.setTiltTargetAngle(targetData.getAngle());
+
+      panTrim = SmartDashboard.getNumber("Pan Trim", panTrim);
+
+      limelightXAngle = Robot.camera.getXAngle();
+      Robot.turret.setPanTarget(limelightXAngle + Robot.turret.getPanAngle() + panTrim);
+
+      Robot.flywheel.setTarget(targetData.getSpeed());
+
+    }
+    Robot.turret.PIDDrive(true);
   }
 }
